c50694ab676954273927dde44730f2faa5470d8d
Lazarus Long <github@roxsoft.co.uk>
2025-11-18 23:21:07 +0000

    CSS no longer has a sizes map

--- lib/HTML/Filter.pm
+++ lib/HTML/Filter.pm
@@ -1,7 +1,7 @@
 package HTML::Filter;
 
 use 5.010001;
-use version; our $VERSION = qv( sprintf '0.1.%d', q$Rev: 13 $ =~ /\d+/gmx );
+use version; our $VERSION = qv( sprintf '0.1.%d', q$Rev: 14 $ =~ /\d+/gmx );
 
 use Moo;
 
